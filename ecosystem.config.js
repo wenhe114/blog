a8b6e1dcdce9ed3cc8715845cddb74484a30f335
wenhe <13884188370@163.com>
2021-12-16 14:10:22 +0800

    修改部署配置
    Signed-off-by: wenhe <13884188370@163.com>

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,16 +7,16 @@ module.exports = {
     instances: 'max', // or an integer
   }],
 
-  deploy: {
-    production: {
-      user: 'SSH_USERNAME',
-      host: 'SSH_HOSTMACHINE',
-      ref: 'origin/master',
-      repo: 'GIT_REPOSITORY',
-      path: 'DESTINATION_PATH',
-      'pre-deploy-local': '',
-      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': ''
-    }
-  }
+  // deploy: {
+  //   production: {
+  //     user: 'SSH_USERNAME',
+  //     host: 'SSH_HOSTMACHINE',
+  //     ref: 'origin/master',
+  //     repo: 'GIT_REPOSITORY',
+  //     path: 'DESTINATION_PATH',
+  //     'pre-deploy-local': '',
+  //     'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
+  //     'pre-setup': ''
+  //   }
+  // }
 };
